9a04bd1a454e22277804ac9058ab76114e7e9842
Jordan Ott <jordanott365@gmail.com>
2017-02-28 16:40:14 -0800

    Update Build.java

--- Build.java
+++ Build.java
@@ -204,7 +204,6 @@ public class Build {
                             }
                         }
                     }
-                    visited.clear();
                 }
 
                 visited.clear();
